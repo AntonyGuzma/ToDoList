0a89e7524ddc79bd2b3bb769778bd42b84038401
Antony _Gusmão <88747866+AntonyGuzma@users.noreply.github.com>
2024-07-25 11:12:19 -0400

    :sparkles: feat: adicionado componentes na aplicação

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-import { Cabecalho } from './components'
+import { Cabecalho, Conteudo } from './components'
 
 const App = () =>  {
 
   return (
     <>
-      <h1>Hello Word 1</h1>
-      <Cabecalho/>
+      <Cabecalho nomeUsuario='Joao'/>
+      <Conteudo>
+        <h1>Titulo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores velit quisquam neque quos! Deleniti delectus, velit, quia quae voluptate officiis voluptatum nihil ipsa sapiente dolorem atque accusamus quos, id nisi.</p>
+      </Conteudo>
     </>
   )
 }
--- vite-project/src/components/Cabecalho/Cabecalho.jsx
+++ vite-project/src/components/Cabecalho/Cabecalho.jsx
@@ -1,10 +1,21 @@
 
-function Cabecalho() {
-  return (
-    <div>
-      Cabecalho
-    </div>
-  )
+// function Cabecalho(props) {
+//   return (
+//     <div>
+//         Bem vindo, {props.nomeUsuario}
+//     </div>
+//   )
+// }
+
+const Cabecalho = (props) => {
+
+    const { nomeUsuario } = props
+
+    return (
+        <div>
+            Bem vindo, {nomeUsuario}
+        </div>
+      ) 
 }
 
 // export nomeado
